47ee755e27deb8125d3d41f4fd01cb7c3838bba0
ddangme <ssilver121@naver.com>
2025-04-02 11:30:44 +0900

    feat: configure router connection

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,12 +2,14 @@ import { Routes as RouterRoutes, Route } from 'react-router-dom';
 import Layout from '@/shared/components/Layout';
 import HomePage from '@/pages/Home';
 import NotFoundPage from '@/pages/NotFound';
+import LoginPage from '@/pages/Login.tsx';
 
 export const Routes = () => {
     return (
         <RouterRoutes>
             <Route path="/" element={<Layout />}>
                 <Route index element={<HomePage />} />
+                <Route path="*" element={<LoginPage />} />
                 <Route path="*" element={<NotFoundPage />} />
             </Route>
         </RouterRoutes>
